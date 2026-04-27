b84d6b4305bd53cad57cc4b5b03cbf92f1862d61
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-04-27 20:34:05 +0600

    Header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,8 +1,21 @@
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format } from "date-fns";
 
- 
 const Header = () => {
     return (
-         <h1>Hello</h1>
+        <div className="text-center py-12 space-y-1">
+            <Image
+                src={logo}
+                width={300}
+                height={200}
+                alt="Logo"
+                className="mx-auto"
+            />
+
+            <p className="font-bold text-xl"> Journalism Without Fear or Favour</p>
+            <p className="font-bold"> {format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
+        </div>
     );
 };
 
